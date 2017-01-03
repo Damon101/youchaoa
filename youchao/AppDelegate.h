a7863005b655d494710939f6be5227eb623e0308
张威 <kirito@zhangweideMac.local>
2017-01-03 17:02:37 +0800

    Initial Commit

---
+++ youchao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  youchao
+//
+//  Created by 张威 on 2017/1/3.
+//  Copyright © 2017年 zw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ youchao/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  youchao
+//
+//  Created by 张威 on 2017/1/3.
+//  Copyright © 2017年 zw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
